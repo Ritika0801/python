dff255c43362a9c71f5af249565674cd62e0f527
Ritika0801 <73068493+Ritika0801@users.noreply.github.com>
2021-08-04 13:49:17 +0530

    Create spyro.py

---
+++ spyro.py
@@ -0,0 +1,11 @@
+import turtle as tt
+tt.bgcolor('black')
+tt.pensize(2)
+tt.speed(20)
+
+for i in range(6):
+    for color in ('red','magenta','blue','cyan','green','white','yellow'):
+        tt.color(color)
+        tt.circle(100)
+        tt.left(10)
+tt.hideturtle()
